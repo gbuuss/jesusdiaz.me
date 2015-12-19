64746b54468748d65cb2b343ef243121d2f70904
Jesús Díaz <gbuuss@gmail.com>
2015-12-19 13:45:37 -0300

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name jesusdiazmeApp
+ * @description
+ * # jesusdiazmeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('jesusdiazmeApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jesusdiazmeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jesusdiazmeApp
+ */
+angular.module('jesusdiazmeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
