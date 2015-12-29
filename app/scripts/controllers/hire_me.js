919f26e67d739a74fd2117fa0a608a92bcdaceff
Jesús Díaz <gbuuss@gmail.com>
2015-12-28 23:55:18 -0300

    Adding footer

---
+++ app/scripts/controllers/hire_me.js
@@ -0,0 +1,13 @@
+(function (){
+
+	'use strict';
+
+	function HireMeCtrl(scope){
+		scope.date = new Date();
+	}
+
+	HireMeCtrl.$inject = ['$scope'];
+
+	angular.module('jesusdiaz')
+	  .controller('HireMeCtrl', HireMeCtrl);
+})();
