e8b6b6058f3b24be9e34dccc34d8b346a7978365
Jesús Díaz <gbuuss@gmail.com>
2016-01-11 20:50:03 -0300

    Adding languageSwitch

--- app/scripts/directives/jsNavbar/jsNavbar.js
+++ app/scripts/directives/jsNavbar/jsNavbar.js
@@ -2,13 +2,25 @@
 
     'use strict';
 
+    function HorizontalBarController(scope, translate){
+
+    	scope.changeLanguage = function(langKey){
+    		translate.use(langKey);
+    	}
+    }
 
     function HorizontalBar(){
         return {
             restrict: 'E',
             templateUrl: 'views/navbar.html',
+            controller: HorizontalBarController
         };
     }
+
+    HorizontalBarController.$inject = [
+    	'$scope',
+    	'$translate'
+    ];
 
     angular.module('jesusdiaz.directives.jsNavbar', [])
         .directive('jsNavbar', HorizontalBar);
