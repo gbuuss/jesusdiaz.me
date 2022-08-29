38b261ac3eb78c09fbaa4a8c7837089c72e87fdb
Jesus Diaz <jesus@pinata.cloud>
2022-08-29 15:02:00 -0300

    trigger deployment

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          jesusdiaz.me
         </a>
       </header>
     </div>
